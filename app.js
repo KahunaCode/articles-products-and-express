698f4c525d7ef21b16174b8f004499449e0ff5e0
James Moore <jameslmoore@gmail.com>
2017-07-15 18:40:18 -1000

    basic products

--- app.js
+++ app.js
@@ -2,9 +2,24 @@
 
 const express = require('express');
 const app = express();
+const bp = require('body-parser');
 const PORT = process.env.PORT || 3000;
 
+app.use(bp.json());
 
+//{ name: String, price: String, inventory: String }
+
+
+app.post('/products', (req,res) => {
+  if (typeof req.body.name === "string" && typeof req.body.price === "string"){
+    console.log(req.body);
+    res.send(`{"success":true}`);
+  }
+  else{
+    console.log('bad post criteria');
+    res.send(`{"success":false}`);
+  }
+});
 
 
 
