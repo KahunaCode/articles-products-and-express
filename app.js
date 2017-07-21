0f4863ae0b8dcb674116e687cfba71b9d0f2261a
James Moore <jameslmoore@gmail.com>
2017-07-20 22:14:31 -1000

    put is putting, need to save it

--- app.js
+++ app.js
@@ -5,13 +5,26 @@ const app = express();
 const exphbs = require('express-handlebars');
 const bp = require('body-parser');
 const fs = require('fs');
+const methodOverride = require('method-override');
 const PORT = process.env.PORT || 3000;
 
 const articles = require('./routes/articles.js');
 const products = require('./routes/products.js');
 
-app.use(bp.json());
+app.use(bp.urlencoded());
 app.use(express.static('products'));
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+app.use(methodOverride(function (req, res) {
+  console.log('please work', req.body);
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method;
+    console.log(method);
+    delete req.body._method;
+    return method;
+  }
+}));
 
 const hbs = exphbs.create({
   defaultLayout: 'main',
--- routes/products.js
+++ routes/products.js
@@ -16,6 +16,23 @@ router
   const product = DS_Products.getProductById(productId);
   console.log(product);
   res.render('home', product);
+})
+.get('/', (req,res) => {
+  const allProducts = DS_Products.getAllProducts();
+  console.log('get all products', allProducts);
+  res.render('index', allProducts);
+})
+.get('/:id/edit', (req,res) => {
+  console.log('got an edit request');
+  console.log('id is', req.params.id);
+  const productId = {id: Number(req.params.id)};
+  res.render('edit', productId);
+})
+.put('/:id/edit', (req,res) => {
+  console.log('got an put request');
+  console.log('id is', req.params.id);
+  const productId = {id: Number(req.params.id)};
+  res.render('edit', productId);
 });
 
 module.exports = router;
